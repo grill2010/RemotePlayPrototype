a0d862678639baa0449241e13cc61e520b5e0490
Florian Grill <f.grill160@gmail.com>
2019-05-28 00:12:20 +0200

    +) Add some draft docoumentation about control messages and workflow +) Improve combobox network adapter initialization

---
+++ Information/Infos.md
@@ -0,0 +1,40 @@
+# UDP protocol
+
+The ps4 udp protocol is a reliable udp flow which means every message (Control messages) needs to be acknowledge by the other party.
+
+## Control messages
+
+The control messages are used to set up the udp remote play protocol and for additional messages between client and PS4 while streaming. The first 4 init Control messages are special and not all rules apply to them. The following exlanation is for control messages after the Init Control messages.
+
+### ReceiverId
+
+The first anwer of from the PS4 to the first Init Control message has a FuncIncr value which the client uses throughout the connection for its control message ReceiverId.
+
+The first Init Control message sent from the client to the PS and its FuncIncr value is the value the PS4 will use for its ReceiverId in the control messages. (It seems to be the value is always 00004823)
+
+### Crypoto
+
+That's not clear yet what this value is used for, but it could be some random number which is then used for the IV counter.
+
+### TagPos
+
+Not yet clear. Seems to be used when the crypoto value is used. TagPos is contained in other messages as well.
+
+### Flag1
+
+It seems to be that control messages which are sent for acknowleding data have a Flag1 value of 3, and control messages which are used for sending new data have a Flag1 value of 0.
+
+### Protobuff Flag
+If the value is 1 then it has a TakionMessage payload in it otherwhise 0
+
+### PLoad Size
+
+Is the size of the payload but it is not yet clear how the size is calculated.
+
+### Func Incr
+
+The Func Incr value is used to to find the matching acknowledge control message to the request. The acknwoledge message should have the same Func Incr value as the request. For the PS4 and the client the FuncIncr value gets increased by one by for each message sent. The client and the PS4 both have unique FuncIncr values the FuncIncr value of the client is usally 00004823 and gets increased by one after the BigPayload is sent.
+
+### Class(?)
+
+It seems to be that control messages which are sent for acknowleding data have a class value of 00019000, and control messages which are used for sending new data have a class value of 00090000.
--- Ps4RemotePlayPrototype/Ps4RemotePlay.Ui/Form1.cs
+++ Ps4RemotePlayPrototype/Ps4RemotePlay.Ui/Form1.cs
@@ -31,9 +31,9 @@ namespace Ps4RemotePlay.Ui
         private readonly PS4DiscoveryService _ps4DiscoveryService;
         private readonly PS4ConnectionService _ps4ConnectionService;
 
-        private LivePcapContext _livePcapContext;
+        private readonly LivePcapContext _livePcapContext;
 
-        private readonly List<LivePacketDevice> networkAdapters = new List<LivePacketDevice>();
+        private readonly List<LivePacketDevice> _networkAdapters = new List<LivePacketDevice>();
 
         public Form1()
         {
@@ -258,7 +258,7 @@ namespace Ps4RemotePlay.Ui
         private void button4_Click(object sender, EventArgs e)
         {
             int selectedIndex = this.comboBoxNetworkAdapter.SelectedIndex;
-            if (selectedIndex >= 0 && selectedIndex <= (this.networkAdapters.Count - 1))
+            if (selectedIndex >= 0 && selectedIndex <= (this._networkAdapters.Count - 1))
             {
                 DisableLivePcapParsingButton();
                 DisablePcapButton();
@@ -266,7 +266,7 @@ namespace Ps4RemotePlay.Ui
                 comboBoxNetworkAdapter.Enabled = false;
                 Task.Factory.StartNew(() =>
                 {
-                    LivePacketDevice selectedDevice = this.networkAdapters[selectedIndex];
+                    LivePacketDevice selectedDevice = this._networkAdapters[selectedIndex];
                     // 65536 guarantees that the whole packet will be captured on all the link layers
                     using (PacketCommunicator communicator = selectedDevice.Open(65536, PacketDeviceOpenAttributes.Promiscuous, 1000))
                     {
@@ -282,7 +282,7 @@ namespace Ps4RemotePlay.Ui
                 });
                 Task.Factory.StartNew(() =>
                 {
-                    LivePacketDevice selectedDevice = this.networkAdapters[selectedIndex];
+                    LivePacketDevice selectedDevice = this._networkAdapters[selectedIndex];
 
                     // 65536 guarantees that the whole packet will be captured on all the link layers
                     using (PacketCommunicator communicator = selectedDevice.Open(65536, PacketDeviceOpenAttributes.Promiscuous, 1000))
@@ -422,15 +422,23 @@ namespace Ps4RemotePlay.Ui
             }
             else
             {
-                networkAdapters.AddRange(allDevices);
-                foreach (var networkAdapter in networkAdapters)
+                _networkAdapters.AddRange(allDevices);
+                foreach (var networkAdapter in _networkAdapters)
                 {
                     string description = networkAdapter.Description ?? networkAdapter.Name;
                     description = description.Replace("Network adapter", "");
-                    description += $" ({networkAdapter.Addresses.Last().Address})";
+                    if (description.Length > 25)
+                    {
+                        description = description.Substring(0, 25);
+                        description += "...";
+                    }
+
+                    if (networkAdapter.Addresses.Count > 0)
+                    {
+                        description += $" ({networkAdapter.Addresses.Last().Address})";
+                    }
                     this.comboBoxNetworkAdapter.Items.Add(description);
                 }
-
             }
         }
 
